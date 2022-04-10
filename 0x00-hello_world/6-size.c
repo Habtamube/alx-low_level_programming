21ac70a7c84e1be1753117539f4738c9cdc1f86f
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-04-11 00:58:00 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,4 @@
 #include <stdio.h> 
-/** 
- * main - Prints out the sizes of various data types on the computer 
- * this program is compiled on. 
- * Return: 0 if success. 
- */ 
 int main(void) 
  { 
         char c; 
