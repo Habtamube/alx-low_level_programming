35d1680777e219e1df3575e990a872172e2315b4
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-04-12 00:00:27 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,9 @@ int main(void)
 char c;
 int i;
 long l;
-  long long ll;
+long long ll;
 float f;
+  
 printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(l));
