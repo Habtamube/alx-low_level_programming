f626dc073ac2ebbac904d24c51154124660aa3f3
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-04-27 20:54:04 +0300

    Update 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - a function that prints a string, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
