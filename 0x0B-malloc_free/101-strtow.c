e37d81f329092d5a223a93e2e3195bb42144fc6a
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-05-04 00:00:37 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,31 +8,27 @@
  *@av: arguments
  * Return: a pointer to a new string
  */
-char **strtow(char *str)
-	
+char **strtow(char *str)	
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+int i, w, j, k, count, m, wordf;
+char **p;
+char *x;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
+w = 0;
+j = 0;
+i = 0;
+count = 0;
 	
-	if (*str == '\0' || str == NULL)
-		return (NULL);
-	
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
+if (*str == '\0' || str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
+	w++;
 	}
 	p = (char **)malloc((w + 1) * sizeof(char *));
-	
 	if (p == NULL)
 		return (NULL);
-	
 	for (wordf = 0; str[wordf] && j <= w; wordf++)
 	{
 		count = 0;
