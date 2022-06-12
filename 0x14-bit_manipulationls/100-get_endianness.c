a641849f0e578fd84432c0ff7c75ba1fd6559ca1
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-06-12 23:06:27 +0300

    Update 100-get_endianness.c

--- 0x14-bit_manipulationls/100-get_endianness.c
+++ 0x14-bit_manipulationls/100-get_endianness.c
@@ -8,11 +8,7 @@
  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	unsigned int n = 1;
 
-	if (*endian == 1)
-		return (1);
-
-	return (0);
+	return (*(char *)(&n) == 1);
 }
