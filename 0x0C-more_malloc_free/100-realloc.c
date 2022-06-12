b03a787a59ae0df67fab2f669aaf3ffab134eb60
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-06-12 19:54:20 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,14 +1,15 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
 
 /**
- *
- *
- *
- *
- *
- */
+* simple_print_buffer - prints buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
