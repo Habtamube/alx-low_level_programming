d34bba9ecb9453589ccb69419f760cf8002d05c0
HABTAMU BERHANU <101341580+Habtamube@users.noreply.github.com>
2022-06-13 20:47:13 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,16 @@
 *main print - string _putchar
 *return: 0
 */
-int main (void)
+int main(void)
 {
-int i;
-string c[]=("_putchar")
-for(i=0,i<8,i++)
-{
-_putchar(c[])
-}
-printf('\n')
-return (0)
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
